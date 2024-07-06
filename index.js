8d9044b52d787ace9669e675e28a8e379c18d672
CHRIS <johnjosh1116@gmail.com>
2024-07-06 22:13:08 +0800

    1

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
-var app = express();
 
+var app = express();
+app.get('/', (req, res)=>{
+   res.send("WELCOME TO HOME");
+});
 app.get('/home', (req, res)=>{
    res.send("WELCOME TO HOME");
 });
 app.get('/about', (req, res)=>{
-   res.send("WELCOME TO ABOUT US, This is a test only");
+   app.use('/', "/Test/About.html")
 });
 
 app.listen(3000);
