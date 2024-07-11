9446fef11152e887bdc9862dc7b0a48b3e2f54bd
CHRIS <johnjosh1116@gmail.com>
2024-07-11 19:53:54 +0800

    2312

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,7 @@ function router() {
   const router = express.Router();
   router.use("/", require("./home"));
   router.use("/about", require("./about"));
+  router.use("/toprank", require("./toprank"));
 
   return router;
 }
---
+++ src/routes/toprank.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/*", async (req, res) => {
+  try {
+    res.render("../Test/toprank", {
+      req,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+module.exports = router;
