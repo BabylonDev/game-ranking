cf1079bb5bb465b4788a6ad08d786cba9958d3c0
CHRIS <johnjosh1116@gmail.com>
2024-07-14 12:02:28 +0800

    231

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,6 +6,9 @@ function router() {
   router.use("/about", require("./about"));
   router.use("/toprank", require("./toprank"));
   router.use("/portfolio", require("./portfolio"));
+  router.use("/topics", require("./topics"));
+  router.use("/community", require("./community"));
+  router.use("/contact", require("./contact"));
   return router;
 }
 
